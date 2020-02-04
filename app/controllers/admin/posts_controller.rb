9aca40b089cfb444cc1cf316060725ae77051730
Admiraltonkey <AdmiralTonkey@gmail.com>
2020-02-04 22:15:16 +0300

    first commit

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,43 @@
+class Admin::PostsController < Admin::AdminController
+  before_action :set_post, only: [:edit, :update, :destroy]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, success: 'Компания успешно создана'
+    else
+      flash.now[:danger] = 'Компания не создана'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post, success: 'Компания успешно обновлена'
+    else
+      flash.now[:danger] = 'Компания не обновлена'
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, success: 'Компания успешно удалена'
+  end
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :summary, :body, :image, :all_tags, :category_id)
+  end
+end
